ed3874c32b7b720908596a721c3d372d7ae6a572
Alessandra Chardon <achardon@ucla.edu>
2021-11-19 11:09:47 -0500

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,50 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  //Do the below if you want to use the useEffect
+  // useEffect(() => {
+  //   if (filters.type === 'all') {
+  //     fetch(`http://localhost:3001/pets`)
+  //     .then(r => r.json())
+  //     .then(data => setPets(data))
+  //   }
+  //   else {
+  //   fetch(`http://localhost:3001/pets?type=${filters.type}`)
+  //   .then(r => r.json())
+  //   .then(data => setPets(data))
+  //   }
+  // }, [filters])
+
+  function onChangeType(e) {
+    setFilters({type: `${e.target.value}`})
+  }
+
+  function onFindPetsClick() {
+    if (filters.type === 'all') {
+      fetch(`http://localhost:3001/pets`)
+      .then(r => r.json())
+      .then(data => setPets(data))
+    }
+    else {
+    fetch(`http://localhost:3001/pets?type=${filters.type}`)
+    .then(r => r.json())
+    .then(data => setPets(data))
+    }
+  }
+
+  function onAdoptPet(petID) {
+    const updatedPets = pets.map(pet => {
+      if (petID === pet.id) {
+        pet.isAdopted = !pet.isAdopted
+        return pet
+      }
+      else {
+        return pet
+      }
+    })
+    setPets(updatedPets)
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +59,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
